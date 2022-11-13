48a3be9ff09b2df7d65adc7d307226f4a39de702
Mudabbirul Islam <71183979+MudabbirulSaad@users.noreply.github.com>
2022-11-13 10:49:46 +0600

    vendorsetup.sh: update path update.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,18 @@
 # Device Tree
 echo "Cloning OnePlus 5T Tree"
 rm -rf device/oneplus/dumpling
-git clone --depth=1 https://github.com/MudabbirulSaad/device_oneplus_dumpling -b elixir device/oneplus/dumpling
+git clone --depth=1 https://github.com/ProjectElixir-Devices/device_oneplus_dumpling -b elixir device/oneplus/dumpling
 echo ""
 
 # Device Tree
 echo "Cloning msm8998-common Tree"
 rm -rf device/oneplus/msm8998-common
-git clone --depth=1 https://github.com/MudabbirulSaad/device_oneplus_msm8998-common -b A13ELXR device/oneplus/msm8998-common
+git clone --depth=1 https://github.com/ProjectElixir-Devices/device_oneplus_msm8998-common -b A13ELXR device/oneplus/msm8998-common
 echo ""
 
 echo "Cloning Vendor blobs"
 rm -rf vendor/oneplus
-git clone --depth=1 https://github.com/MudabbirulSaad/vendor_oneplus_dumpcheese -b A13 vendor/oneplus
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_oneplus_dumpcheese -b A13 vendor/oneplus
 echo ""
 
 echo "Cloning kernel tree"
